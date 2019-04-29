224e1fa7290e27e7a0f84e8ee18fa7a48232012e
Gunkut Yunak <yunakg@rferl.org>
2019-04-29 10:17:58 +0200

    Initial commit

---
+++ src/SemanticVersioning.Host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SemanticVersioning.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
