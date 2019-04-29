962fb24c879f02ff162fcca42b31d1e6f49cfd9f
Gunkut Yunak <yunakg@rferl.org>
2019-04-29 12:01:14 +0200

    Commit on directly master.

---
+++ src/SemanticVersioning.Core/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace SemanticVersioning.Core
+{
+    public class TestClass: ITestInterface
+    {
+        public Task DoSomething()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DoSomethingMore()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
